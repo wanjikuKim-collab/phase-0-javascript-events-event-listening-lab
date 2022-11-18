5f898be73992c3759df356bf0fd639996edcff68
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-11-18 22:16:39 +0300

    Completed text

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    let inputElement = document.getElementById('button');
+
+    function clickInput(){
+        inputElement.innerHTML = 'I was clicked!'
+    }
+
+    inputElement.addEventListener('click',clickInput );    
 }
+
+addingEventListener();
